b702ba422d01138ef47cd0636bc5426a4b333cc8
MeganiumMute <150499376+MeganiumMute@users.noreply.github.com>
2024-09-26 21:32:44 +0500

    Add files via upload

---
+++ clear.cmd
@@ -0,0 +1,11 @@
+ rem Removing untracked directories
+
+git clean -f -d CHtoto
+
+rem Removing ignored directories
+
+git clean -d -X -f CHtoto
+
+rem Removing ignored files
+
+git clean -f -x CHtoto
